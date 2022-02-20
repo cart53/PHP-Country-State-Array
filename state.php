d01b75de2b117ceae2579f50c80f55679e51a7ef
Cart53 - Online Shopping Solution <94687531+cart53@users.noreply.github.com>
2022-02-20 16:00:24 +0530

    Created State Array at state.php

---
+++ state.php
@@ -0,0 +1,907 @@
+<?php
+return [
+    "AF" => [
+
+    ],
+    "AL" => [
+
+    ],
+    "DZ" => [
+
+    ],
+    "AS" => [
+
+    ],
+    "AD" => [
+
+    ],
+    "AO" => [
+
+    ],
+    "AI" => [
+
+    ],
+    "AQ" => [
+
+    ],
+    "AG" => [
+
+    ],
+    "AR" => [
+
+    ],
+    "AM" => [
+
+    ],
+    "AW" => [
+
+    ],
+    "AU" => [
+        'AAT' => 'Australian Antarctic Territory',
+        'ACT' => 'Australian Capital Territory',
+        'JBT' => 'Jervis Bay Territory',
+        'NSW' => 'New South Wales',
+        'NT' => 'Northern Territory',
+        'QLD' => 'Queensland',
+        'SA' => 'South Australia',
+        'TAS' => 'Tasmania',
+        'VIC' => 'Victoria',
+        'WA' => 'Western Australia'
+    ],
+    "AT" => [
+
+    ],
+    "AZ" => [
+
+    ],
+    "BS" => [
+
+    ],
+    "BH" => [
+
+    ],
+    "BD" => [
+
+    ],
+    "BB" => [
+
+    ],
+    "BY" => [
+
+    ],
+    "BE" => [
+
+    ],
+    "BZ" => [
+
+    ],
+    "BJ" => [
+
+    ],
+    "BM" => [
+
+    ],
+    "BT" => [
+
+    ],
+    "BO" => [
+
+    ],
+    "BA" => [
+
+    ],
+    "BW" => [
+
+    ],
+    "BV" => [
+
+    ],
+    "BR" => [
+
+    ],
+    "BQ" => [
+
+    ],
+    "IO" => [
+
+    ],
+    "VG" => [
+
+    ],
+    "BN" => [
+
+    ],
+    "BG" => [
+
+    ],
+    "BF" => [
+
+    ],
+    "BI" => [
+
+    ],
+    "KH" => [
+
+    ],
+    "CM" => [
+
+    ],
+    "CA" => [
+        'AB' => 'Alberta',
+        'BC' => 'British Columbia',
+        'MB' => 'Manitoba',
+        'NB' => 'New Brunswick',
+        'NF' => 'Newfoundland and Labrador',
+        'NT' => 'Northwest Territories',
+        'NS' => 'Nova Scotia',
+        'NU' => 'Nunavut',
+        'ON' => 'Ontario',
+        'PE' => 'Prince Edward Island',
+        'QC' => 'Quebec',
+        'SK' => 'Saskatchewan',
+        'YT' => 'Yukon Territory'
+    ],
+    "CT" => [
+
+    ],
+    "CV" => [
+
+    ],
+    "KY" => [
+
+    ],
+    "CF" => [
+
+    ],
+    "TD" => [
+
+    ],
+    "CL" => [
+
+    ],
+    "CN" => [
+
+    ],
+    "CX" => [
+
+    ],
+    "CC" => [
+
+    ],
+    "CO" => [
+
+    ],
+    "KM" => [
+
+    ],
+    "CG" => [
+
+    ],
+    "CD" => [
+
+    ],
+    "CK" => [
+
+    ],
+    "CR" => [
+
+    ],
+    "HR" => [
+
+    ],
+    "CU" => [
+
+    ],
+    "CY" => [
+
+    ],
+    "CZ" => [
+
+    ],
+    "CI" => [
+
+    ],
+    "DK" => [
+
+    ],
+    "DJ" => [
+
+    ],
+    "DM" => [
+
+    ],
+    "DO" => [
+
+    ],
+    "NQ" => [
+
+    ],
+    "DD" => [
+
+    ],
+    "EC" => [
+
+    ],
+    "EG" => [
+
+    ],
+    "SV" => [
+
+    ],
+    "GQ" => [
+
+    ],
+    "ER" => [
+
+    ],
+    "EE" => [
+
+    ],
+    "ET" => [
+
+    ],
+    "FK" => [
+
+    ],
+    "FO" => [
+
+    ],
+    "FJ" => [
+
+    ],
+    "FI" => [
+
+    ],
+    "FR" => [
+
+    ],
+    "GF" => [
+
+    ],
+    "PF" => [
+
+    ],
+    "TF" => [
+
+    ],
+    "FQ" => [
+
+    ],
+    "GA" => [
+
+    ],
+    "GM" => [
+
+    ],
+    "GE" => [
+
+    ],
+    "DE" => [
+
+    ],
+    "GH" => [
+
+    ],
+    "GI" => [
+
+    ],
+    "GR" => [
+
+    ],
+    "GL" => [
+
+    ],
+    "GD" => [
+
+    ],
+    "GP" => [
+
+    ],
+    "GU" => [
+
+    ],
+    "GT" => [
+
+    ],
+    "GG" => [
+
+    ],
+    "GN" => [
+
+    ],
+    "GW" => [
+
+    ],
+    "GY" => [
+
+    ],
+    "HT" => [
+
+    ],
+    "HM" => [
+
+    ],
+    "HN" => [
+
+    ],
+    "HK" => [
+
+    ],
+    "HU" => [
+
+    ],
+    "IS" => [
+
+    ],
+    "IN" => [
+        'AN' => 'Andaman & Nicobar',
+        'AP' => 'Andhra Pradesh',
+        'AR' => 'Arunachal Pradesh',
+        'AS' => 'Assam',
+        'BR' => 'Bihar',
+        'CH' => 'Chandigarh',
+        'CT' => 'Chhattisgarh',
+        'DD' => 'Daman & Diu',
+        'DL' => 'Delhi',
+        'DN' => 'Dadra and Nagar Haveli',
+        'GA' => 'Goa',
+        'GJ' => 'Gujarat',
+        'HR' => 'Haryana',
+        'HP' => 'Himachal Pradesh',
+        'JK' => 'Jammu & Kashmir',
+        'JH' => 'Jharkhand',
+        'KA' => 'Karnataka',
+        'KL' => 'Kerala',
+        'LD' => 'Lakshadweep',
+        'MP' => 'Madhya Pradesh',
+        'MH' => 'Maharashtra',
+        'MN' => 'Manipur',
+        'ML' => 'Meghalaya',
+        'MZ' => 'Mizoram',
+        'NL' => 'Nagaland',
+        'OR' => 'Odisha',
+        'PB' => 'Punjab',
+        'PY' => 'Puducherry',
+        'RJ' => 'Rajasthan',
+        'SK' => 'Sikkim',
+        'TN' => 'Tamil Nadu',
+        'TR' => 'Tripura',
+        'UK' => 'Uttarakhand',
+        'UP' => 'Uttar Pradesh',
+        'WB' => 'West Bengal',
+    ],
+    "ID" => [
+
+    ],
+    "IR" => [
+
+    ],
+    "IQ" => [
+
+    ],
+    "IE" => [
+
+    ],
+    "IM" => [
+
+    ],
+    "IL" => [
+
+    ],
+    "IT" => [
+
+    ],
+    "JM" => [
+
+    ],
+    "JP" => [
+
+    ],
+    "JE" => [
+
+    ],
+    "JT" => [
+
+    ],
+    "JO" => [
+
+    ],
+    "KZ" => [
+
+    ],
+    "KE" => [
+
+    ],
+    "KI" => [
+
+    ],
+    "KW" => [
+
+    ],
+    "KG" => [
+
+    ],
+    "LA" => [
+
+    ],
+    "LV" => [
+
+    ],
+    "LB" => [
+
+    ],
+    "LS" => [
+
+    ],
+    "LR" => [
+
+    ],
+    "LY" => [
+
+    ],
+    "LI" => [
+
+    ],
+    "LT" => [
+
+    ],
+    "LU" => [
+
+    ],
+    "MO" => [
+
+    ],
+    "MK" => [
+
+    ],
+    "MG" => [
+
+    ],
+    "MW" => [
+
+    ],
+    "MY" => [
+
+    ],
+    "MV" => [
+
+    ],
+    "ML" => [
+
+    ],
+    "MT" => [
+
+    ],
+    "MH" => [
+
+    ],
+    "MQ" => [
+
+    ],
+    "MR" => [
+
+    ],
+    "MU" => [
+
+    ],
+    "YT" => [
+
+    ],
+    "FX" => [
+
+    ],
+    "MX" => [
+
+    ],
+    "FM" => [
+
+    ],
+    "MI" => [
+
+    ],
+    "MD" => [
+
+    ],
+    "MC" => [
+
+    ],
+    "MN" => [
+
+    ],
+    "ME" => [
+
+    ],
+    "MS" => [
+
+    ],
+    "MA" => [
+
+    ],
+    "MZ" => [
+
+    ],
+    "MM" => [
+
+    ],
+    "NA" => [
+
+    ],
+    "NR" => [
+
+    ],
+    "NP" => [
+
+    ],
+    "NL" => [
+
+    ],
+    "AN" => [
+
+    ],
+    "NT" => [
+
+    ],
+    "NC" => [
+
+    ],
+    "NZ" => [
+
+    ],
+    "NI" => [
+
+    ],
+    "NE" => [
+
+    ],
+    "NG" => [
+
+    ],
+    "NU" => [
+
+    ],
+    "NF" => [
+
+    ],
+    "KP" => [
+
+    ],
+    "VD" => [
+
+    ],
+    "MP" => [
+
+    ],
+    "NO" => [
+
+    ],
+    "OM" => [
+
+    ],
+    "PC" => [
+
+    ],
+    "PK" => [
+
+    ],
+    "PW" => [
+
+    ],
+    "PS" => [
+
+    ],
+    "PA" => [
+
+    ],
+    "PZ" => [
+
+    ],
+    "PG" => [
+
+    ],
+    "PY" => [
+
+    ],
+    "YD" => [
+
+    ],
+    "PE" => [
+
+    ],
+    "PH" => [
+
+    ],
+    "PN" => [
+
+    ],
+    "PL" => [
+
+    ],
+    "PT" => [
+
+    ],
+    "PR" => [
+
+    ],
+    "QA" => [
+
+    ],
+    "RO" => [
+
+    ],
+    "RU" => [
+
+    ],
+    "RW" => [
+
+    ],
+    "RE" => [
+
+    ],
+    "BL" => [
+
+    ],
+    "SH" => [
+
+    ],
+    "KN" => [
+
+    ],
+    "LC" => [
+
+    ],
+    "MF" => [
+
+    ],
+    "PM" => [
+
+    ],
+    "VC" => [
+
+    ],
+    "WS" => [
+
+    ],
+    "SM" => [
+
+    ],
+    "SA" => [
+
+    ],
+    "SN" => [
+
+    ],
+    "RS" => [
+
+    ],
+    "CS" => [
+
+    ],
+    "SC" => [
+
+    ],
+    "SL" => [
+
+    ],
+    "SG" => [
+
+    ],
+    "SK" => [
+
+    ],
+    "SI" => [
+
+    ],
+    "SB" => [
+
+    ],
+    "SO" => [
+
+    ],
+    "ZA" => [
+
+    ],
+    "GS" => [
+
+    ],
+    "KR" => [
+
+    ],
+    "ES" => [
+
+    ],
+    "LK" => [
+
+    ],
+    "SD" => [
+
+    ],
+    "SR" => [
+
+    ],
+    "SJ" => [
+
+    ],
+    "SZ" => [
+
+    ],
+    "SE" => [
+
+    ],
+    "CH" => [
+
+    ],
+    "SY" => [
+
+    ],
+    "ST" => [
+
+    ],
+    "TW" => [
+
+    ],
+    "TJ" => [
+
+    ],
+    "TZ" => [
+
+    ],
+    "TH" => [
+
+    ],
+    "TL" => [
+
+    ],
+    "TG" => [
+
+    ],
+    "TK" => [
+
+    ],
+    "TO" => [
+
+    ],
+    "TT" => [
+
+    ],
+    "TN" => [
+
+    ],
+    "TR" => [
+
+    ],
+    "TM" => [
+
+    ],
+    "TC" => [
+
+    ],
+    "TV" => [
+
+    ],
+    "UM" => [
+
+    ],
+    "PU" => [
+
+    ],
+    "VI" => [
+
+    ],
+    "UG" => [
+
+    ],
+    "UA" => [
+
+    ],
+    "SU" => [
+
+    ],
+    "AE" => [
+
+    ],
+    "GB" => [
+
+    ],
+    "US" => [
+        'AL' => 'Alabama',
+        'AK' => 'Alaska',
+        'AS' => 'American Samoa',
+        'AZ' => 'Arizona',
+        'AR' => 'Arkansas',
+        'AE' => 'Armed Forces - Europe',
+        'AP' => 'Armed Forces - Pacific',
+        'AA' => 'Armed Forces - USA/Canada',
+        'CA' => 'California',
+        'CO' => 'Colorado',
+        'CT' => 'Connecticut',
+        'DE' => 'Delaware',
+        'DC' => 'District of Columbia',
+        'FL' => 'Florida',
+        'GA' => 'Georgia',
+        'GU' => 'Guam',
+        'HI' => 'Hawaii',
+        'ID' => 'Idaho',
+        'IL' => 'Illinois',
+        'IN' => 'Indiana',
+        'IA' => 'Iowa',
+        'KS' => 'Kansas',
+        'KY' => 'Kentucky',
+        'LA' => 'Louisiana',
+        'ME' => 'Maine',
+        'MD' => 'Maryland',
+        'MA' => 'Massachusetts',
+        'MI' => 'Michigan',
+        'MN' => 'Minnesota',
+        'MS' => 'Mississippi',
+        'MO' => 'Missouri',
+        'MT' => 'Montana',
+        'NE' => 'Nebraska',
+        'NV' => 'Nevada',
+        'NH' => 'New Hampshire',
+        'NJ' => 'New Jersey',
+        'NM' => 'New Mexico',
+        'NY' => 'New York',
+        'NC' => 'North Carolina',
+        'ND' => 'North Dakota',
+        'OH' => 'Ohio',
+        'OK' => 'Oklahoma',
+        'OR' => 'Oregon',
+        'PA' => 'Pennsylvania',
+        'PR' => 'Puerto Rico',
+        'RI' => 'Rhode Island',
+        'SC' => 'South Carolina',
+        'SD' => 'South Dakota',
+        'TN' => 'Tennessee',
+        'TX' => 'Texas',
+        'UT' => 'Utah',
+        'VT' => 'Vermont',
+        'VI' => 'Virgin Islands',
+        'VA' => 'Virginia',
+        'WA' => 'Washington',
+        'WV' => 'West Virginia',
+        'WI' => 'Wisconsin',
+        'WY' => 'Wyoming'
+    ],
+    "ZZ" => [
+        
+    ],
+    "UY" => [
+        
+    ],
+    "UZ" => [
+        
+    ],
+    "VU" => [
+        
+    ],
+    "VA" => [
+        
+    ],
+    "VE" => [
+        
+    ],
+    "VN" => [
+        
+    ],
+    "WK" => [
+        
+    ],
+    "WF" => [
+        
+    ],
+    "EH" => [
+        
+    ],
+    "YE" => [
+        
+    ],
+    "ZM" => [
+        
+    ],
+    "ZW" => [
+        
+    ],
+    "AX" => [
+        
+    ],
+];
